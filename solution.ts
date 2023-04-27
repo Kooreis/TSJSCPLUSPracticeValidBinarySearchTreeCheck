49110f1c15caf6fee394d9325e3b64a6f5c5ae88
Kooreis <kooreis.official@gmail.com>
2023-04-27 18:16:15 -0500

    This update introduces the isValidBST function, which checks if a binary tree is a valid binary search tree. It uses recursion to traverse the tree and checks if each node's value is within the valid range.

--- solution.ts
+++ solution.ts
@@ -1,11 +1,11 @@
-class TreeNode {
-    value: number;
-    left: TreeNode | null;
-    right: TreeNode | null;
-
-    constructor(value: number, left: TreeNode | null = null, right: TreeNode | null = null) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
+function isValidBST(root: TreeNode | null, min: number = Number.MIN_SAFE_INTEGER, max: number = Number.MAX_SAFE_INTEGER): boolean {
+    if (root === null) {
+        return true;
     }
+
+    if (root.value <= min || root.value >= max) {
+        return false;
+    }
+
+    return isValidBST(root.left, min, root.value) && isValidBST(root.right, root.value, max);
 }
